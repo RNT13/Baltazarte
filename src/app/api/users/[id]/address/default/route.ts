4ff90b78ae7207f1bd1b7253e8d93fe55c0c32c5
RNT13 <renatornt13@gmail.com>
2025-09-24 16:05:16 -0300

    fix: - Correção nas rotas de api para verção do next 15.5

--- src/app/api/users/[id]/address/default/route.ts
+++ src/app/api/users/[id]/address/default/route.ts
@@ -1,9 +1,9 @@
 import { prisma } from '@/utils/prisma'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 // GET endereço padrão
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  const { id } = await params
+export async function GET(req: NextRequest, params: { params: Promise<{ id: string }> }) {
+  const { id } = await params.params
   if (!id) {
     return NextResponse.json({ error: 'ID do usuário é obrigatório' }, { status: 400 })
   }
